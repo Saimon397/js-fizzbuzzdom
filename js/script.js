ae8b3ee4819e437e3eb841f65e56fc7f9809ae31
Saimon397 <simonecarrabba@gmail.com>
2022-10-10 17:51:26 +0200

    Finito

--- js/script.js
+++ js/script.js
@@ -3,20 +3,25 @@ const container = document.querySelector("div.container");
 let type;
 let text;
 
-for (let i = 0; i <= 101; i++) {
+for (let i = 0; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         type = 'fizz_buzz';
         text = 'FizzBuzz';
-    } else if (i % 3 === 0) {
+    }
+    else if (i % 3 === 0) {
         type = 'fizz';
         text = 'Fizz';
-    } else if (i % 5 === 0) {
+    }
+    else if (i % 5 === 0) {
         type = 'buzz';
         text = 'Buzz';
-    } else {
+    }
+    else {
         type = '';
         text = i;
     }
+
     const box = `<div class="box ${type}"> ${text}</div>`;
     container.innerHTML += box;
 }
+
