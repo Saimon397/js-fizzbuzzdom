936695730cac0cc88defff5a5830121c802406a4
Saimon397 <simonecarrabba@gmail.com>
2022-10-10 17:26:48 +0200

    Work_done

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const container = document.querySelector("div.container");
+
+let type;
+let text;
+
+for (let i = 0; i <= 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        type = 'fizz_buzz';
+        text = 'FizzBuzz';
+    } else if (i % 3 === 0) {
+        type = 'fizz';
+        text = 'Fizz';
+    } else if (i % 5 === 0) {
+        type = 'buzz';
+        text = 'Buzz';
+    } else {
+        type = '';
+        text = i;
+    }
+    const box = `<div class="box ${type}"> ${text}</div>`;
+    container.innerHTML += box;
+}
